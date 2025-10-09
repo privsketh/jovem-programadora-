2b2eb3d67c8649606749e809d6844ac5f80b45ba
privsketh <kerollyn324@gmail.com>
2025-10-08 21:35:21 -0300

    aula 1

--- JovemProgamadoraJava/src/jovemprogamadorajava/JovemProgamadoraJava.java
+++ JovemProgamadoraJava/src/jovemprogamadorajava/JovemProgamadoraJava.java
@@ -1,9 +1,17 @@
 
 package jovemprogamadorajava;
 
-public class JovemProgamadoraJava 
+public class JovemProgamadoraJava  {
    
     public static void main(String[] args) {
+        
+        String nome = "Kérollyn Pires"; 
+        int ano = 2025,anoVelho;
+        anoVelho = ano- 2009;
+     
+        
+       
+        System.out.println("Voce tera"+ anoVelho + "anos" );
  
     }
     
