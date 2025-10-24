61df1381bd9f09dec862bf54499462c4bca0ab7e
privsketh <kerollyn324@gmail.com>
2025-10-23 21:18:02 -0300

    numero

---
+++ JovemProgamadoraJava/src/jovemprogamadorajava/exemploWHILE.java
@@ -0,0 +1,18 @@
+
+package jovemprogamadorajava;
+
+
+public class exemploWHILE {
+    
+    public static void main(String[] args) {
+        int contador = 1;
+        
+        while (contador <= 10) {
+            System.out.print(contador+ "");
+            contador++;
+            
+        }
+        
+        }
+    }
+    
